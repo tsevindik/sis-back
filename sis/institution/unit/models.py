2b52bd29afc1831d6ab1b71df4d6543e6cc16e07
İhsan Seçer <abdullahsecer@std.sehir.edu.tr>
2016-11-14 23:13:33 +0300

    refs #1: add academicunit and academicunittype model and migrate

--- sis/institution/unit/models.py
+++ sis/institution/unit/models.py
@@ -1,3 +1,21 @@
+from django.utils.translation import ugettext_lazy as _
 from django.db import models
 
-# Create your models here.
+from ..models import University
+
+
+class AcademicUnitType(models.Model):
+    name = models.CharField(
+        max_length=50,
+        verbose_name=_("İsim")
+    )
+
+
+class AcademicUnit(models.Model):
+    name = models.CharField(
+        max_length=150,
+        verbose_name=_("İsim")
+    )
+    university = models.ForeignKey(University)
+    type = models.ForeignKey(AcademicUnitType)
+    description = models.TextField(verbose_name=_("Açıklama"))
