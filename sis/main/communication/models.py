dd66b026b008e4ad9506392f6ac807765a613417
İhsan Seçer <abdullahsecer@std.sehir.edu.tr>
2016-11-15 14:06:25 +0300

    refs #1: add models related to communication

--- sis/main/communication/models.py
+++ sis/main/communication/models.py
@@ -1,3 +1,54 @@
+from django.utils.translation import ugettext_lazy as _
 from django.db import models
 
-# Create your models here.
+from ...utils.time.models import TimeStamp
+from ...utils.models import Type
+
+
+class Country(TimeStamp):
+    name = models.CharField(
+        max_length=50,
+        verbose_name=_("İsim")
+    )
+
+
+class City(TimeStamp):
+    name = models.CharField(
+        max_length=50,
+        verbose_name=_("İsim")
+    )
+    country = models.ForeignKey(Country)
+
+
+class District(TimeStamp):
+    city = models.ForeignKey(City)
+    name = models.CharField(
+        max_length=50,
+        verbose_name=_("İsim")
+    )
+
+
+class AddressType(Type):
+    pass
+
+
+class Address(TimeStamp):
+    type = models.ForeignKey(AddressType)
+    district = models.ForeignKey(District)
+    postcode = models.IntegerField(
+        max_length=10,
+        verbose_name=_("Posta Kodu")
+    )
+    description = models.TextField(verbose_name=_("Açıklama"))
+
+
+class PhoneType(Type):
+    pass
+
+
+class Phone(TimeStamp):
+    type = models.ForeignKey(PhoneType)
+    number = models.CharField(
+        max_length=30,
+        verbose_name=_("Numara")
+    )
