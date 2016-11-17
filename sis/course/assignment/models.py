80ec96e0478bff1588c779317a59bcfef6aa213a
İhsan Seçer <abdullahsecer@std.sehir.edu.tr>
2016-11-17 20:14:43 +0300

    refs #1: add assignment related models

--- sis/course/assignment/models.py
+++ sis/course/assignment/models.py
@@ -1,3 +1,29 @@
+from django.utils.translation import ugettext_lazy as _
 from django.db import models
 
-# Create your models here.
+from ...common.models.common import Type
+from ...common.models.schedule import Event
+from ...institution.schedule.models import AcademicSemester
+from ..session.models import CourseSession
+
+
+ASSIGNMENT_IMPLEMENTATION = (
+    (0, _('Uzak')),
+    (1, _('Yerel'))
+)
+
+
+class AssignmentType(Type):
+    pass
+
+
+class Assignment(Event):
+    type = models.ForeignKey(AssignmentType)
+    course_session = models.ForeignKey(CourseSession)
+    semester = models.ForeignKey(AcademicSemester)
+    percentage = models.IntegerField(verbose_name=_("Yüzde"))
+    implementation = models.CharField(
+        max_length=1,
+        choices=ASSIGNMENT_IMPLEMENTATION,
+        verbose_name=_("Uygulama")
+    )
