c7c0943c64e1d874da043d3db6166c385108e0e0
İhsan Seçer <abdullahsecer@std.sehir.edu.tr>
2016-11-17 14:17:47 +0300

    refs #1: add campus, campusaddress and place models

--- sis/institution/facility/models.py
+++ sis/institution/facility/models.py
@@ -1,3 +1,27 @@
+from django.utils.translation import ugettext_lazy as _
 from django.db import models
 
-# Create your models here.
+from ...common.models.time import TimeStamp
+from ...common.models.communication import Address
+
+
+class CampusAddress(Address):
+    pass
+
+
+class Campus(TimeStamp):
+    name = models.CharField(max)
+
+
+class Place(TimeStamp):
+    campus = models.ForeignKey(Campus)
+    name = models.CharField(
+        max_length=100,
+        verbose_name=_("İsim"),
+    )
+    code = models.CharField(
+        max_length=20,
+        verbose_name=_("Kod")
+    )
+    floor = models.IntegerField(verbose_name=_("Kat"))
+    capacity = models.IntegerField(verbose_name=_("Kapasite"))
