890bf5cc67b112d74465fdcbf05e34e513d9439b
İhsan Seçer <abdullahsecer@std.sehir.edu.tr>
2016-11-17 17:11:30 +0300

    refs #1: add course session related models

--- sis/course/session/models.py
+++ sis/course/session/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from ...common.models.time import TimeStamp, DayTimeInterval
+from ...common.models.schedule import PlaceEvent
+from ...institution.schedule.models import AcademicSemester
+from ...institution.facility.models import Place
+from ...user.user.models import User
+from ..course.models import Course
+
+
+class CourseSession(TimeStamp):
+    course = models.ForeignKey(Course)
+    semester = models.ForeignKey(AcademicSemester)
+
+
+class CourseSessionInstructor(TimeStamp):
+    course_session = models.ForeignKey(CourseSession)
+    instructor = models.ForeignKey(User)
+
+
+class CourseSessionLesson(DayTimeInterval):
+    course_session = models.ForeignKey(CourseSession)
+    place = models.ForeignKey(Place)
+
+
+class CourseSessionLessonMeeting(PlaceEvent):
+    course_session_lesson = models.ForeignKey(CourseSessionLesson)
