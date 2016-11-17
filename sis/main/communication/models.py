31a4a95f18a45efbcf7820f7cd0a17fdf467aa7e
İhsan Seçer <abdullahsecer@std.sehir.edu.tr>
2016-11-17 11:51:48 +0300

    refs #1: move telephone and address models to common, change them as abstract base model, delete type models

--- sis/main/communication/models.py
+++ sis/main/communication/models.py
@@ -2,7 +2,6 @@ from django.utils.translation import ugettext_lazy as _
 from django.db import models
 
 from ...common.models.time import TimeStamp
-from ...common.models.common import Type
 
 
 class Country(TimeStamp):
@@ -25,29 +24,4 @@ class District(TimeStamp):
     name = models.CharField(
         max_length=50,
         verbose_name=_("İsim")
-    )
-
-
-class AddressType(Type):
-    pass
-
-
-class Address(TimeStamp):
-    type = models.ForeignKey(AddressType)
-    district = models.ForeignKey(District)
-    postcode = models.IntegerField(
-        verbose_name=_("Posta Kodu")
-    )
-    description = models.TextField(verbose_name=_("Açıklama"))
-
-
-class PhoneType(Type):
-    pass
-
-
-class Phone(TimeStamp):
-    type = models.ForeignKey(PhoneType)
-    number = models.CharField(
-        max_length=30,
-        verbose_name=_("Numara")
     )
