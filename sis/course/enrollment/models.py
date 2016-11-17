733d78382161d748d71e595e9d86454e3c7ac209
İhsan Seçer <abdullahsecer@std.sehir.edu.tr>
2016-11-17 20:15:13 +0300

    refs #1: add enrollment related models

--- sis/course/enrollment/models.py
+++ sis/course/enrollment/models.py
@@ -1,3 +1,22 @@
+from django.utils.translation import ugettext_lazy as _
 from django.db import models
 
-# Create your models here.
+from ...common.models.schedule import Attendance
+from ...common.models.time import TimeStamp
+from ...user.user.models import User
+from ..session.models import CourseSession, CourseSessionLessonMeeting
+
+
+class CourseSessionEnrollment(TimeStamp):
+    course_session = models.ForeignKey(CourseSession)
+    student = models.ForeignKey(User)
+
+
+class LessonStudentAttendance(Attendance):
+    course_session_lesson_meeting = models.ForeignKey(CourseSessionLessonMeeting)
+    student = models.ForeignKey(User)
+
+
+class EnrollmentGrade(TimeStamp):
+    enrollment = models.ForeignKey(CourseSessionEnrollment)
+    grade = models.IntegerField(verbose_name=_("Not"))
