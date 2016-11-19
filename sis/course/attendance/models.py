00e11883d731ab658610bf1ebdc08920659bd1de
İhsan Seçer <abdullahsecer@std.sehir.edu.tr>
2016-11-19 17:37:00 +0300

    refs #1: add instructor attendance model

--- sis/course/attendance/models.py
+++ sis/course/attendance/models.py
@@ -8,3 +8,8 @@ from ..session.models import LessonEvent
 class StudentLessonAttendance(Attendance):
     event = models.ForeignKey(LessonEvent)
     student = models.ForeignKey(User)
+
+
+class InstructorLessonAttendance(Attendance):
+    event = models.ForeignKey(LessonEvent)
+    instructor = models.ForeignKey(User)
