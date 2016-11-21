39c0ec1fe7b20b5080ff063373e824cc3cf41aab
İhsan Seçer <abdullahsecer@std.sehir.edu.tr>
2016-11-21 18:26:41 +0300

    refs #1: add user profile models

--- sis/user/student/models.py
+++ sis/user/student/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+from ...common.models.time import TimeStamp
+from ..user.models import User
+
+
+class StudentProfile(TimeStamp):
+    student = models.ForeignKey(User)
