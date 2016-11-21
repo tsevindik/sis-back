55de14656a3ef37c47f1eacbf7fd51c83ad8d39e
İhsan Seçer <abdullahsecer@std.sehir.edu.tr>
2016-11-21 13:37:12 +0300

    refs #1: add registry grade model

--- sis/course/registry/models.py
+++ sis/course/registry/models.py
@@ -3,10 +3,17 @@ from django.db import models
 
 from ...common.models.time import TimeStamp
 from ...user.user.models import User
-from ..session.models import CourseSession
+from ..session.models import CourseSession, SessionLetterGrade
 
 
 class SessionRegistry(TimeStamp):
     session = models.ForeignKey(CourseSession)
     student = models.ForeignKey(User)
 
+
+class RegistryGrade(TimeStamp):
+    registry = models.ForeignKey(SessionRegistry)
+    numeric_grade = models.FloatField(verbose_name=_("Sayısal Not"))
+    letter_grade = models.ForeignKey(SessionLetterGrade)
+    rank = models.IntegerField(verbose_name=_("Sıra"))
+    percentile = models.FloatField(verbose_name=_("Yüzdelik Dilim"))
