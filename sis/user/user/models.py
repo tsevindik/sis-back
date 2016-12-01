3588d38171af1783b11401e579f6667fe668988a
İhsan Seçer <abdullahsecer@std.sehir.edu.tr>
2016-12-01 15:24:27 +0300

    refs #6: add user university model

--- sis/user/user/models.py
+++ sis/user/user/models.py
@@ -2,6 +2,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from ...institute.institute.models import University
 from ...common.models.time import TimeStamp
 
 
@@ -14,3 +15,9 @@ class User(AbstractUser):
 
 class UserProfile(TimeStamp):
     user = models.ForeignKey(User)
+
+
+class UserUniversity(TimeStamp):
+    user = models.ForeignKey(User)
+    university = models.ForeignKey(University)
+    is_primary = models.BooleanField()
