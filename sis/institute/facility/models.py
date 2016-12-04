80cf378df509ec30c550f164cd9a25c2014cbf1c
İhsan Seçer <abdullahsecer@std.sehir.edu.tr>
2016-12-04 23:04:52 +0300

    add building and block, rename campusplace to room

--- sis/institute/facility/models.py
+++ sis/institute/facility/models.py
@@ -10,6 +10,10 @@ class CampusAddress(Address):
     pass
 
 
+class BuildingAddress(Address):
+    pass
+
+
 class UniversityCampus(TimeStamp):
     university = models.ForeignKey(
         University,
@@ -25,11 +29,36 @@ class UniversityCampus(TimeStamp):
     )
 
 
-class CampusPlace(TimeStamp):
+class CampusBuilding(TimeStamp):
+    name = models.CharField(
+        max_length=100,
+        verbose_name=_("İsim"),
+    )
     university_campus = models.ForeignKey(
         UniversityCampus,
         verbose_name=_("Kampüs")
     )
+    building_address = models.ForeignKey(
+        BuildingAddress,
+        verbose_name=_("Bina Adresi")
+    )
+    has_blocks = models.BooleanField(
+        verbose_name=_("Blok")
+    )
+
+
+class BuildingBlock(TimeStamp):
+    name = models.CharField(
+        max_length=100,
+        verbose_name=_("İsim"),
+    )
+    campus_building = models.ForeignKey(
+        CampusBuilding,
+        verbose_name=_("Kampüs Binası")
+    )
+
+
+class BuildingRoom(TimeStamp):
     name = models.CharField(
         max_length=100,
         verbose_name=_("İsim"),
@@ -43,4 +72,19 @@ class CampusPlace(TimeStamp):
     )
     capacity = models.IntegerField(
         verbose_name=_("Kapasite")
+    )
+    campus_building = models.ForeignKey(
+        CampusBuilding,
+        verbose_name=_("Kampüs Binası")
+    )
+
+
+class BlockRoom(TimeStamp):
+    building_block = models.ForeignKey(
+        BuildingBlock,
+        verbose_name=_("Blok")
+    )
+    building_room = models.ForeignKey(
+        BuildingRoom,
+        verbose_name=_("Oda")
     )
