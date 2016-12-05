2d3f4d266b234c7584ce4ba59470e87b70f674e3
İhsan Seçer <abdullahsecer@std.sehir.edu.tr>
2016-12-05 21:59:47 +0300

    add program adviser model

--- sis/user/instructor/models.py
+++ sis/user/instructor/models.py
@@ -1,6 +1,9 @@
 from django.utils.translation import ugettext_lazy as _
 from django.db import models
 
+from ...user.student.models import StudentProgram
+from ...common.models.time import TimeStamp
+from ...user.user.models import User
 from ...common.models.user import UserProfile, UserAddress, UserPhone
 
 
@@ -14,3 +17,14 @@ class InstructorPhone(UserPhone):
 
 class InstructorAddress(UserAddress):
     pass
+
+
+class ProgramAdviser(TimeStamp):
+    user = models.ForeignKey(
+        User,
+        verbose_name=_("Kullanıcı")
+    )
+    student_program = models.ForeignKey(
+        StudentProgram,
+        verbose_name=_("Öğrenci Programı")
+    )
