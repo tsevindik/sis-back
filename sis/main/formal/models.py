2825a4b037b3440789f53f1c5e6b93dcdc115ae3
İhsan Seçer <abdullahsecer@std.sehir.edu.tr>
2016-12-05 22:24:11 +0300

    add certification model

--- sis/main/formal/models.py
+++ sis/main/formal/models.py
@@ -1,2 +1,18 @@
+from django.utils.translation import ugettext_lazy as _
 from django.db import models
 
+from ...common.models.time import TimeStamp
+
+
+class Certification(TimeStamp):
+    name = models.CharField(
+        max_length=150,
+        verbose_name=_("İsim")
+    )
+    abbreviation = models.CharField(
+        max_length=150,
+        verbose_name=_("Kısaltma")
+    )
+    out_of_grade = models.IntegerField(
+        verbose_name=_("Not Üzerinden")
+    )
