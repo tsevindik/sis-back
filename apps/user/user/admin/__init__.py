60d086bb1b9cdb639ce9147e48050caae036f380
İhsan Seçer <abdullahsecer@std.sehir.edu.tr>
2017-01-12 09:30:25 +0300

    change permission related models, add json field

--- apps/user/user/admin/__init__.py
+++ apps/user/user/admin/__init__.py
@@ -1,4 +1,3 @@
 from .main import UserAdmin, UserUniversityAdmin, WorkStatusAdmin
 from .trans import WorkStatusTransAdmin
 from .auth import UserPermissionAdmin
-from .row_auth import UniversityAuthAdmin, UnitAuthAdmin, ProgramAuthAdmin, CourseAuthAdmin, SectionAuthAdmin
--- apps/user/user/models/auth.py
+++ apps/user/user/models/auth.py
@@ -1,42 +1,32 @@
 from django.utils.translation import ugettext_lazy as _
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
+from config.settings.common import AUTH_USER_MODEL
 from utils.models import time as time_models
-from . import main
 
 
 class UserPermission(time_models.TimeStamp):
+    """
+    Attributes:
+        user: user having specified permissions
+        data: all permission related data
+            Sample JSON:
+                {
+                    rowLevelPermission: {
+                        isPermitted: boolean
+                        forAll: boolean
+                        permittedIds: [number]
+                    }, ...
+                    , modelLevelPermission: boolean
+                }
+            Row Level Permissions: university, unit, program, course, section
+            Model Level Permissions: nothing for now
+    """
     user = models.OneToOneField(
-        main.User,
+        AUTH_USER_MODEL,
         verbose_name=_("Kullanıcı")
     )
-    university = models.BooleanField(
-        verbose_name=_("Üniversite")
-    )
-    all_universities = models.BooleanField(
-        verbose_name=_("Bütün Üniversiteler")
-    )
-    unit = models.BooleanField(
-        verbose_name=_("Birim")
-    )
-    all_units = models.BooleanField(
-        verbose_name=_("Bütün Birimler")
-    )
-    program = models.BooleanField(
-        verbose_name=_("Program")
-    )
-    all_programs = models.BooleanField(
-        verbose_name=_("Bütün Programlar")
-    )
-    course = models.BooleanField(
-        verbose_name=_("Ders")
-    )
-    all_courses = models.BooleanField(
-        verbose_name=_("Bütün Dersler")
-    )
-    section = models.BooleanField(
-        verbose_name=_("Ders Grubu")
-    )
-    all_sections = models.BooleanField(
-        verbose_name=_("Bütün Ders Grupları")
+    data = JSONField(
+        verbose_name=_("Veri")
     )
