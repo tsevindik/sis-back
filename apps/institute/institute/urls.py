bc16f836f38429a5ac0d3909ff4e5d88d6ffbae3
İhsan Seçer <abdullahsecer@std.sehir.edu.tr>
2017-01-16 12:04:19 +0300

    edit urls

--- apps/institute/institute/urls.py
+++ apps/institute/institute/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from .views import UniversityConfigApi
 
 urlpatterns = [
-    url(r'^university-config/$', UniversityConfigApi.as_view()),
+    url(r'^university-config/$', UniversityConfigApi.as_view(), name="university_config"),
 ]
--- apps/user/user/urls.py
+++ apps/user/user/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
 urlpatterns = [
-    url(r'^login/', obtain_jwt_token),
-    url(r'^token-refresh/', refresh_jwt_token),
-    url(r'^token-verify/', verify_jwt_token)
+    url(r'^authenticate/', obtain_jwt_token, name="authenticate"),
+    url(r'^refresh-token/', refresh_jwt_token, name="refresh_token"),
+    url(r'^verify-token/', verify_jwt_token, name="verify_token")
 ]
