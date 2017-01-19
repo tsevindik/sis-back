8a0ac7b77299684ab81e9784c0ca5770c6187b54
İhsan Seçer <abdullahsecer@std.sehir.edu.tr>
2017-01-19 12:33:45 +0300

    configure settings for production

--- config/settings/common.py
+++ config/settings/common.py
@@ -9,13 +9,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!(+akxq6q0c38can89@(#ga$b9jx5cs*y(k+hv0_zd1wyy#qt='
+SECRET_KEY = os.getenv('DJANGO_DEBUG', '!(+akxq6q0c38can89@(#ga$b9jx5cs*y(k+hv0_zd1wyy#qt=')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '192.168.1.47']
-
+DEBUG = os.getenv('DJANGO_DEBUG', False)
 
 # Default Django apps:
 DJANGO_APPS = (
--- config/settings/local.py
+++ config/settings/local.py
@@ -1,1 +1,5 @@
 from .common import *
+
+DEBUG = os.getenv('DJANGO_DEBUG', True)
+
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', ['localhost', '127.0.0.1', '0.0.0.0'])
